105e4d5b1b14547cf641229045d177441c53d03d
Brian Hendel <brian.hendel@gmail.com>
2019-08-08 15:03:05 -0400

    for heroku build 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -40,7 +41,8 @@ library.add(faUserCircle);
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,      
+    BrowserAnimationsModule,     
+    FormsModule,
     AngularMaterialModule,
     FlexLayoutModule,
     AppRoutingModule,
